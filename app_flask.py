3bd80ea051acd0ae288b81ee93ea09861bff808a
Laxmi Khilnani <138607032+laxmikhilnani20@users.noreply.github.com>
2026-03-28 20:31:16 +0530

    Change app run method to use Flask's built-in server instead of SocketIO

--- app_flask.py
+++ app_flask.py
@@ -379,4 +379,4 @@ def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
